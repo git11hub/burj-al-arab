22edd0e7f51bc614e6702602aba41ca95c77fc71
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-03-18 00:10:51 +0600

    second commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB1XLUP7dvpSUtPyZl59-2h16tYPMg0Gsg",
+    authDomain: "burj-al-arap.firebaseapp.com",
+    projectId: "burj-al-arap",
+    storageBucket: "burj-al-arap.appspot.com",
+    messagingSenderId: "745181807910",
+    appId: "1:745181807910:web:9a49e8082adc1124abade7"
+  };
+  export default firebaseConfig;
